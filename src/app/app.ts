1f44125baf2465c8c7f19aaafd8309c6a270a6c8
bhuwan poudel <bkspoudel@gmail.com>
2026-02-19 15:46:49 +0100

    add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Container } from './shared/container/container';
+import { Body } from './components/body/body';
+import { Keyboard } from './components/keyboard/keyboard';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Container, Body, Keyboard, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
