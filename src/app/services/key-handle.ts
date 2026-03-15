5496b5605791e55a9775237dd82124fb32726462
bhuwan poudel <bkspoudel@gmail.com>
2026-03-15 10:56:04 +0100

    add documentation for the service countKey()

--- src/app/services/key-handle.ts
+++ src/app/services/key-handle.ts
@@ -11,6 +11,16 @@ export class KeyHandle {
   keyStroke = new BehaviorSubject<number>(0);
   keysList = new Subject<string[]>();
 
+  /**
+   * Takes a key as input and updates the key stroke count and the list of keys pressed. It emits the update count and the list of keys through the respective subject.
+   * @param key
+   * @example
+   * // Create an instance of KeyHandle
+   * const keyHandle = new KeyHandle();
+   * // Count a key press
+   * keyHandle.countKey('a');
+   */
+
   countKey(key: string) {
     this.count += 1;
     this.keys.push(key);
