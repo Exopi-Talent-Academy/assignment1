6d0fb0671949d43566353e2ad79490f5a3c11366
bhuwan poudel <bkspoudel@gmail.com>
2026-03-15 10:47:10 +0100

    add comments

--- src/app/services/key-handle.spec.ts
+++ src/app/services/key-handle.spec.ts
@@ -3,12 +3,17 @@ import { TestBed } from '@angular/core/testing';
 import { KeyHandle } from './key-handle';
 
 describe('KeyHandle', () => {
+  //Arrangement
   let service: KeyHandle;
+
+  //Action
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(KeyHandle);
   });
+
+  //Assertion
 
   it('should be created', () => {
     expect(service).toBeTruthy();
